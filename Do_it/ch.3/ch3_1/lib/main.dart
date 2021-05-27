7810f6f2d39ee7bab8702c00fad1090c51a44f1e
SinJ <johankim27@gmail.com>
2021-05-27 14:30:52 +0900

    0527 | Sinbad | edit ch3_1

--- Do_it/ch.3/ch3_1/lib/main.dart
+++ Do_it/ch.3/ch3_1/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-// 데모 앱 수정
+// 데모 앱 수정 예제
 void main() {
   runApp(MyApp());
 }
 
-//Switch 예제////////////////////////////////////////////////////////////////////
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -13,8 +12,12 @@ class MyApp extends StatefulWidget {
   }
 }
 
+//Button 예제////////////////////////////////////////////////////////////////////
 class _MyApp extends State<MyApp> {
   var switchValue = false;
+  String test = 'hello';
+  Color _color = Colors.blue;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,19 +28,61 @@ class _MyApp extends State<MyApp> {
         ),
         darkTheme: ThemeData.light(),
         home: Scaffold(
-          body: Center(
-            child: Switch(
-                value: switchValue,
-                onChanged: (value) {
+            body: Center(
+          child: RaisedButton(
+              child: Text('$test'),
+              color: _color,
+              onPressed: () {
+                if (_color == Colors.blue) {
                   setState(() {
-                    print(value);
-                    switchValue = value;
+                    test = 'flutter';
+                    _color = Colors.amber;
                   });
-                }),
-          ),
-        ));
+                } else {
+                  setState(() {
+                    test = 'hello';
+                    _color = Colors.blue;
+                  });
+                }
+              }),
+        )));
   }
 }
+////////////////////////////////////////////////////////////////////////////////
+
+//Switch 예제////////////////////////////////////////////////////////////////////
+// class MyApp extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//     return _MyApp();
+//   }
+// }
+
+// class _MyApp extends State<MyApp> {
+//   var switchValue = false;
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: 'Flutter Demo',
+//         theme: ThemeData(
+//           primarySwatch: Colors.blue,
+//           visualDensity: VisualDensity.adaptivePlatformDensity,
+//         ),
+//         darkTheme: ThemeData.light(),
+//         home: Scaffold(
+//           body: Center(
+//             child: Switch(
+//                 value: switchValue,
+//                 onChanged: (value) {
+//                   setState(() {
+//                     print(value);
+//                     switchValue = value;
+//                   });
+//                 }),
+//           ),
+//         ));
+//   }
+// }
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
