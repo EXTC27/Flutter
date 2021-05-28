fd41b0d29ad7022c8cac7889c34b681fc8f2a8cf
SinJ <johankim27@gmail.com>
2021-05-28 21:42:35 +0900

    0528 | Sinbad | edit ch5 files

---
+++ Do_it/ch.5/ch5_1/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'sub/firstPage.dart';
+import 'sub/secondPage.dart';
+
+// TabBar로 화면 이동
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('TabBar Example'),
+        ),
+        body: TabBarView(
+          children: <Widget>[FirstApp(), SecondApp()],
+          controller: controller,
+        ),
+        bottomNavigationBar: TabBar(
+          tabs: <Tab>[
+            Tab(
+              icon: Icon(
+                Icons.looks_one,
+                color: Colors.blue,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.looks_two,
+                color: Colors.blue,
+              ),
+            )
+          ],
+          controller: controller,
+        ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+    controller.addListener(() {
+      if (!controller.indexIsChanging) {
+        print('이전 index, ${controller.previousIndex}');
+        print('현재 index, ${controller.index}');
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
